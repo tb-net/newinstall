e3751a7cb291d075d7434aac41827bc208948515
tb-net <thibault@blehaut.net>
2020-10-16 16:17:44 +0200

    adding vim

--- install_vim.sh
+++ install_vim.sh
@@ -3,9 +3,11 @@
 
 apt remove -y vim-tiny
 apt install -y vim
+rm -f .vim
 
 dir=$(pwd)
-\cp -r .vim ~
 \cp .viminfo ~
 \cp .vimrc ~
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
