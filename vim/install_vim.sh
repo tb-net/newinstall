01f53e549ef638647698439b101097aba025bb53
tb-net <thibault@blehaut.net>
2020-10-22 10:43:10 +0200

    .

--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -6,7 +6,7 @@ apt install -y vim
 rm -f .vim
 
 dir=$(pwd)
-\cp .viminfo ~
+#\cp .viminfo ~
 \cp .vimrc ~
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
