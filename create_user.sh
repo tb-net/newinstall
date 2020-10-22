0438828cc5e5b0efbd3c9aa25bdc12c3600c9f5f
tb-net <thibault@blehaut.net>
2020-10-22 18:48:45 +0200

    .

---
+++ create_user.sh
@@ -0,0 +1,15 @@
+# github.com/tb-net
+
+# as root
+groupadd scout
+scoutpwd=$(cat /root/.cred_scout)
+useradd -u 1001 -g scout -d /home/scout -s /bin/bash -p $(echo $scoutpwd | openssl passwd -1 stdin) -m scout
+usermod -aG sudo scout
+echo '# my parameters' >> /etc/sudoers
+echo 'scout ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+chown scout:scout install_remote.sh
+cp install_remote.sh /home/scout/install_remote.sh
+cd /home/scout
+su scout >/dev/null
+
+
--- install_remote.sh
+++ install_remote.sh
@@ -1,16 +1,8 @@
 # github.com/tb-net
 
-# as root
-scoutpwd=$(cat /root/.cred_scout)	
-useradd -u 1001 -g users -d /home/scout -s /bin/bash -p $(echo $scoutpwd | openssl passwd -1 stdin) -m scout
-usermod -aG sudo scout
-echo '# my parameters' >> /etc/sudoers
-echo 'scout ALL=(ALL) NOPASSWD: /usr/bin/apt' >> /etc/sudoers
-su scout
-
 # as user
-mkdir ~/scripts
-cd ~/scripts
+mkdir scripts
+cd scripts
 sudo apt update
 sudo apt install git -y
 git clone https://github.com/tb-net/newinstall.git
