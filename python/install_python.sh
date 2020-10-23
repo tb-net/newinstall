2efe818b79cdf0d08e30664fa08f95ba98f1f538
tb-net <thibault@blehaut.net>
2020-10-23 15:39:37 +0200

    .

--- python/install_python.sh
+++ python/install_python.sh
@@ -19,12 +19,13 @@ wget "https://www.python.org/ftp/python/"$pyver"/Python-"$pyver".tar.xz"
 tar xvf "Python-"$pyver".tar.xz"
 rm "Python-"$pyver".tar.xz"
 cd "Python-"$pyver
-./configure --enable-optimizations --prefix=/usr/local/stow/python$pyshort
+#./configure --enable-optimizations --prefix=/usr/local/stow/python$pyshort
+./configure --prefix=/usr/local/stow/python$pyshort
 make -j 1
 make altinstall
 cd /usr/local/stow/
 stow python$pyshort
-python3.9 -m pip intall --upgrade pip
+python3.9 -m pip install --upgrade pip
 pip3.9 install virtualenv virtualenvwrapper
 mkdir .virtualenvs
 echo "# Virtualenv settings" >> /home/$usr/.bashrc
