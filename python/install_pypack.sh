8b8611f1fb2d88a3a2baa14fbd79ec80a407fc89
tb-net <thibault@blehaut.net>
2020-10-25 19:50:17 +0100

    .

--- python/install_pypack.sh
+++ python/install_pypack.sh
@@ -4,7 +4,7 @@
 pip install ipython
 pip install arrow
 pip install fernet
-pip install beautifulsoup4 selenium
+pip install requests beautifulsoup4 selenium
 pip install pdftotext openpyxl xlrd
 pip install numpy pandas scipy
 
