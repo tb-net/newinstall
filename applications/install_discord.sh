cdb1a31a60f0607a4d15858a06f3eae8091be20e
tb-net <thibault@blehaut.net>
2020-12-07 23:35:47 +0100

    .

--- applications/install_discord.sh
+++ applications/install_discord.sh
@@ -2,7 +2,7 @@
 # github.com/tb-net
 # Run with sudo
 
-wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 
 apt install -y ./discord.deb
 
